96ba795788b84ddad7c67701bfa220f8d9fbc949
Adrian <coding4adrian@yahoo.com>
2021-04-26 13:40:54 -0700

    update

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -42,56 +42,62 @@ export class PlayerComponent implements OnInit {
       this.id = params.id;
     })
     this.DataList.getRoster().subscribe(res => {
-      if(this.player){
-        if(this.player.position === "Pitcher"){
-          this.DataList.getPlayerPitchingSeason(this.id).subscribe(res => {
-            if(res){
-              this.playerPitchingSeason.push(res);
-              this.pitchingSeason = this.playerPitchingSeason[0].sport_pitching_tm.queryResults.row
-            }
-          })
-          this.DataList.getPlayerPitchingCareer(this.id).subscribe(res => {
-            if(res){
-              this.playerPitchingCareer.push(res);
-              this.pitchingCareer = this.playerPitchingCareer[0].sport_career_pitching.queryResults.row
-              this.playerData = true;
-            }
-          })
-          /* this.DataList.getPlayerPitchingPostseason(this.id).subscribe(res => {
-            if(res){
-              this.playerPitchingPostseason.push(res);
-              this.pitchingPost = this.playerPitchingPostseason[0].sport_pitching_tm.queryResults.row
-            }
-          }) */
-          /* this.pitchingSeason = this.player.pitching.regular; */
-          /* this.pitchingCareer = this.player.pitching.career; */
-          this.pitchingPost = this.player.pitching.postseason;
-          this.isPitcher = true;
-        } else {
-          this.DataList.getPlayerHittingSeason(this.id).subscribe(res => {
-            if(res){
-              this.playerHittingSeason.push(res);
-              this.fielderSeason = this.playerHittingSeason[0].sport_hitting_tm.queryResults.row
-            }
-          })
-          this.DataList.getPlayerHittingCareer(this.id).subscribe(res => {
-            if(res){
-              this.playerHittingCareer.push(res);
-              this.fielderCareer = this.playerHittingCareer[0].sport_career_hitting.queryResults.row
-              this.playerData = true;
-            }
-          })
-          /* this.DataList.getPlayerHittingPostseason(this.id).subscribe(res => {
-            if(res){
-              this.playerHittingPostseason.push(res);
-              this.fielderPost = this.playerHittingSeason[0].sport_hitting_tm.queryResults.row
-            }
-          }) */ 
-          /* this.fielderSeason = this.player.hitting.regular; */
-          /* this.fielderCareer = this.player.hitting.career; */
-          this.fielderPost = this.player.hitting.postseason;
-          this.isPitcher = false;
-          
+      if(res){
+        this.roster.push(res);
+        this.roster = this.roster[0];
+        
+        this.player = this.roster.find(player => player.player_id === this.id);
+        if(this.player){
+          if(this.player.position === "Pitcher"){
+            this.DataList.getPlayerPitchingSeason(this.id).subscribe(res => {
+              if(res){
+                this.playerPitchingSeason.push(res);
+                this.pitchingSeason = this.playerPitchingSeason[0].sport_pitching_tm.queryResults.row
+              }
+            })
+            this.DataList.getPlayerPitchingCareer(this.id).subscribe(res => {
+              if(res){
+                this.playerPitchingCareer.push(res);
+                this.pitchingCareer = this.playerPitchingCareer[0].sport_career_pitching.queryResults.row
+                this.playerData = true;
+              }
+            })
+            /* this.DataList.getPlayerPitchingPostseason(this.id).subscribe(res => {
+              if(res){
+                this.playerPitchingPostseason.push(res);
+                this.pitchingPost = this.playerPitchingPostseason[0].sport_pitching_tm.queryResults.row
+              }
+            }) */
+            /* this.pitchingSeason = this.player.pitching.regular; */
+            /* this.pitchingCareer = this.player.pitching.career; */
+            this.pitchingPost = this.player.pitching.postseason;
+            this.isPitcher = true;
+          } else {
+            this.DataList.getPlayerHittingSeason(this.id).subscribe(res => {
+              if(res){
+                this.playerHittingSeason.push(res);
+                this.fielderSeason = this.playerHittingSeason[0].sport_hitting_tm.queryResults.row
+              }
+            })
+            this.DataList.getPlayerHittingCareer(this.id).subscribe(res => {
+              if(res){
+                this.playerHittingCareer.push(res);
+                this.fielderCareer = this.playerHittingCareer[0].sport_career_hitting.queryResults.row
+                this.playerData = true;
+              }
+            })
+            /* this.DataList.getPlayerHittingPostseason(this.id).subscribe(res => {
+              if(res){
+                this.playerHittingPostseason.push(res);
+                this.fielderPost = this.playerHittingSeason[0].sport_hitting_tm.queryResults.row
+              }
+            }) */ 
+            /* this.fielderSeason = this.player.hitting.regular; */
+            /* this.fielderCareer = this.player.hitting.career; */
+            this.fielderPost = this.player.hitting.postseason;
+            this.isPitcher = false;
+            
+          }
         }
       }
     })
