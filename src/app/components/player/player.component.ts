443451aba7585e406601c2f6c13215282abdc258
Adrian <coding4adrian@yahoo.com>
2021-04-26 13:12:21 -0700

    api added

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -18,6 +18,13 @@ export class PlayerComponent implements OnInit {
   fielderCareer = null;
   fielderPost = null;
 
+  playerPitchingSeason = [];
+  playerPitchingCareer = [];
+  playerPitchingPostseason = [];
+  playerHittingSeason = [];
+  playerHittingCareer = [];
+  playerHittingPostseason = [];
+
   playerData = false;
   isPitcher = true;
 
@@ -33,30 +40,60 @@ export class PlayerComponent implements OnInit {
   getPlayerData(){
     this.route.params.subscribe(params => {
       this.id = parseInt(params.id);
-      console.log(this.id);
+      /* console.log(this.id); */
     })
     this.DataList.getRoster().subscribe(res => {
-      if(res){
-        this.roster.push(res);
-        this.roster = this.roster[0];
-        
-        this.player = this.roster.find(player => player.id === this.id);
-        console.log(this.player)
-        if(this.player){
-          if(this.player.position === "Pitcher"){
-            this.pitchingSeason = this.player.pitching.regular;
-            this.pitchingCareer = this.player.pitching.career;
-            this.pitchingPost = this.player.pitching.postseason;
-            this.isPitcher = true;
-          } else {
-            this.fielderSeason = this.player.hitting.regular;
-            this.fielderCareer = this.player.hitting.career;
-            this.fielderPost = this.player.hitting.postseason;
-            this.isPitcher = false;
-          }
+      if(this.player){
+        if(this.player.position === "Pitcher"){
+          this.DataList.getPlayerPitchingSeason(this.id).subscribe(res => {
+            if(res){
+              this.playerPitchingSeason.push(res);
+              this.pitchingSeason = this.playerPitchingSeason[0].sport_pitching_tm.queryResults.row
+            }
+          })
+          this.DataList.getPlayerPitchingCareer(this.id).subscribe(res => {
+            if(res){
+              this.playerPitchingCareer.push(res);
+              this.pitchingCareer = this.playerPitchingCareer[0].sport_career_pitching.queryResults.row
+              this.playerData = true;
+            }
+          })
+          /* this.DataList.getPlayerPitchingPostseason(this.id).subscribe(res => {
+            if(res){
+              this.playerPitchingPostseason.push(res);
+              this.pitchingPost = this.playerPitchingPostseason[0].sport_pitching_tm.queryResults.row
+            }
+          }) */
+          /* this.pitchingSeason = this.player.pitching.regular; */
+          /* this.pitchingCareer = this.player.pitching.career; */
+          this.pitchingPost = this.player.pitching.postseason;
+          this.isPitcher = true;
+        } else {
+          this.DataList.getPlayerHittingSeason(this.id).subscribe(res => {
+            if(res){
+              this.playerHittingSeason.push(res);
+              this.fielderSeason = this.playerHittingSeason[0].sport_hitting_tm.queryResults.row
+            }
+          })
+          this.DataList.getPlayerHittingCareer(this.id).subscribe(res => {
+            if(res){
+              this.playerHittingCareer.push(res);
+              this.fielderCareer = this.playerHittingCareer[0].sport_career_hitting.queryResults.row
+              this.playerData = true;
+            }
+          })
+          /* this.DataList.getPlayerHittingPostseason(this.id).subscribe(res => {
+            if(res){
+              this.playerHittingPostseason.push(res);
+              this.fielderPost = this.playerHittingSeason[0].sport_hitting_tm.queryResults.row
+            }
+          }) */ 
+          /* this.fielderSeason = this.player.hitting.regular; */
+          /* this.fielderCareer = this.player.hitting.career; */
+          this.fielderPost = this.player.hitting.postseason;
+          this.isPitcher = false;
+          
         }
-        this.playerData = true;
-        console.log(this.player)
       }
     })
   }
--- src/app/service/data-load.service.ts
+++ src/app/service/data-load.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 
 interface roster {
   id: number,
+  player_id: string,
   fName: string,
   lName: string,
   number: number,
@@ -30,6 +31,17 @@ export class DataLoadService {
   private scheduleData = "assets/data/schedule.json";
   private scheduleDB = "/api/schedule";
 
+  season = '2021';
+  postGameType = 'D'; /* Change game_type param to 'D' Division, 'F' Wild Card, 'L' League, or 'W' WorldSeries for each level of playoffs */
+
+  private playerPitchingSeason = "http://lookup-service-prod.mlb.com/json/named.sport_pitching_tm.bam?league_list_id='mlb'&game_type='R'"
+  private playerPitchingCareer = "http://lookup-service-prod.mlb.com/json/named.sport_career_pitching.bam?league_list_id='mlb'&game_type='R'&player_id="
+  private playerPitchingPostseason = "http://lookup-service-prod.mlb.com/json/named.sport_pitching_tm.bam?league_list_id='mlb'"
+
+  private playerHittingSeason = "http://lookup-service-prod.mlb.com/json/named.sport_hitting_tm.bam?league_list_id='mlb'&game_type='R'"
+  private playerHittingCareer = "http://lookup-service-prod.mlb.com/json/named.sport_career_hitting.bam?league_list_id='mlb'&game_type='R'&player_id="
+  private playerHittingPostseason = "http://lookup-service-prod.mlb.com/json/named.sport_hitting_tm.bam?league_list_id='mlb'"
+
   constructor(private http: HttpClient) {}
 
   getRoster(){
@@ -44,5 +56,27 @@ export class DataLoadService {
   //Schedule via MongoDB
   getScheduleDB(){
     return this.http.get<schedule>(this.scheduleDB);
+  }
+
+  // Pitching Stats
+  getPlayerPitchingSeason(playerId){
+    return this.http.get(this.playerPitchingSeason + "&season=" + this.season +"&player_id=" + playerId)
+  }
+  getPlayerPitchingCareer(playerId){
+    return this.http.get(this.playerPitchingCareer + playerId)
+  }
+  getPlayerPitchingPostseason(playerId){
+    return this.http.get(this.playerPitchingPostseason + "&game_type=" + this.postGameType + "&season=" + this.season + "&player_id=" + playerId)
+  }
+
+  // Hitting Stats
+  getPlayerHittingSeason(playerId){
+    return this.http.get(this.playerHittingSeason + "&season=" + this.season +"&player_id=" + playerId)
+  }
+  getPlayerHittingCareer(playerId){
+    return this.http.get(this.playerHittingCareer + playerId)
+  }
+  getPlayerHittingPostseason(playerId){
+    return this.http.get(this.playerHittingPostseason + "&game_type=" + this.postGameType + "&season=" + this.season + "&player_id=" + playerId)
   }
 }
